010ea883dd75c9b9f0ba3ec553dd62308dcb78f6
Vincent Nalupta <vnalupta.tech@gmail.com>
2024-01-10 07:41:01 -0500

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   "title": "Snailbites - the digital home of Vincent Nalupta",
   "description": "Snailbites is the digital home of Vincent Nalupta, a UX Engineer in NYC.",
+  "icons": {
+    "icon": '/crown.png',
+  },
 }
 
 export default function RootLayout({
